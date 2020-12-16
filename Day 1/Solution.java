96d27f80b97fdf0a255ec1b40467f57b70a0060f
Andrew Ton <aton4@uci.edu>
2020-12-15 20:29:20 -0800

    Added code comments to Solution.java

--- Day 1/Solution.java
+++ Day 1/Solution.java
@@ -4,10 +4,12 @@ import java.util.List;
 
 class Solution 
 {
+    // This function returns a List<List<Integer>> of all unique quadruplets in int[] nums that sum up to the target integer. Time Complexity: O(n^3)
     public List<List<Integer>> fourSum(int[] nums, int target) 
     {
         ArrayList<List<Integer>> uniqueQuadruplets = new ArrayList<List<Integer>>();
         
+        // sorts int[] nums in ascending order
         Arrays.sort(nums, 0, nums.length);
         
         int first, second, leftIndex, rightIndex;
@@ -19,32 +21,41 @@ class Solution
         
         for (first = 0; first < nums.length; first++)
         {
+            // skips to next interation to avoid duplicate quadruplets by comparing previous index to current index
             if ( (first != 0) && (nums[first - 1] == nums[first]) ) 
                 continue;
             
             for (second = first + 1; second < nums.length - 1; second++)
             {
+                // skips to next interation to avoid duplicate quadruplets by comparing previous index to current index
                 if ( (second != (first + 1)) && (nums[second - 1] == nums[second]) )
                     continue;
                 
+                // gets the two indexes to use in while loop below
                 leftIndex = second + 1;
                 rightIndex = nums.length - 1;
                 
+                // stores the previous indexes for comparision
                 prevLeftVal = Integer.MIN_VALUE;
                 prevRightVal = Integer.MIN_VALUE;
                 
+                // while loop uses two pointer technique to reduce time complexity
                 while (leftIndex < rightIndex)
                 {
+                    // skips to enxt iteration if the left and right values are duplicates
                     if ( (prevLeftVal == nums[leftIndex]) && (prevRightVal == nums[rightIndex]) )
                     {
                         leftIndex++;
                         continue;
                     }
                     
+                    //calculates the quadruple sum
                     quadSum = nums[first] + nums[second] + nums[leftIndex] + nums[rightIndex];
                     
+                    // increments the left pointer if quadSum is less then target
                     if (quadSum < target)
                         leftIndex++;
+                    // if quadSum is equal to target, add the lsit of values to uniqueQuadruplets and save the left and right indexs to prevLeftVal and prevRightVal
                     else if (quadSum == target)
                     {
                         prevLeftVal = nums[leftIndex];
@@ -52,7 +63,8 @@ class Solution
                         uniqueQuadruplets.add( Arrays.asList(nums[first], nums[second], nums[leftIndex], nums[rightIndex]) );
                         leftIndex++;
                     }
-                    else
+                    // decrements the right pointer if quadSUm is greater than target
+                    else 
                         rightIndex--;
                 }
             }
@@ -60,4 +72,5 @@ class Solution
         
         return uniqueQuadruplets;
     }
+
 }
