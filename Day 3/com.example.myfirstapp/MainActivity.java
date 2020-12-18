bba86cc0e4f36904279bf400df6b4d5eb65daebc
Andrew Ton <aton4@uci.edu>
2020-12-18 14:05:13 -0800

    added comments and wrote README.md
    comments are in MainActivity.java

--- Day 3/com.example.myfirstapp/MainActivity.java
+++ Day 3/com.example.myfirstapp/MainActivity.java
@@ -12,36 +12,43 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // sends a message in logcat that the app code is currently in onCreate
         Log.d("tag", "In onCreate");
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+
+        // sends a message in logcat that the app code is currently in onStart
         Log.d("tag", "In onStart");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        // sends a message in logcat that the app code is currently in onResume
         Log.d("tag", "In onResume");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
+        // sends a message in logcat that the app code is currently in onPause
         Log.d("tag", "In onPause");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
+        // sends a message in logcat that the app code is currently in onStop
         Log.d("tag", "In onStop");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        // sends a message in logcat that the app code is currently in onDestroy
         Log.d("tag", "In onDestroy");
     }
 }
